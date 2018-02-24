c94d38fa62b68e20cdeedf6e388ddbb46a177b4b
C.... Z <cihangirzengin@gmail.com>
2018-02-24 12:48:22 +0300

    emacs configuration file horoz.org is added

---
+++ init.el
@@ -0,0 +1,18 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+;; accessing a package repo over https on Windows is a no go, so we
+;; fallback to http there
+(if (eq system-type 'windows-nt)
+    (add-to-list 'package-archives
+                 '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives
+               '("melpa" . "https://melpa.org/packages/") t))
+
+(org-babel-load-file "~/.emacs.d/horoz.org")
+
+;; (load "~/.emacs.d/test.el")
+
+
